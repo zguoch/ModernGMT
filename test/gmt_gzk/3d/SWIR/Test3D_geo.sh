dc1eeeedb13ceefe16c942c96de6dff2e79f3bb9
Zhikui Guo <zguoch@gmail.com>
2018-12-10 23:48:41 +0100

    add SWIR example

--- test/gmt_gzk/3d/SWIR/Test3D_geo.sh
+++ test/gmt_gzk/3d/SWIR/Test3D_geo.sh
@@ -10,7 +10,7 @@ figset
 datapath
 dataset
 # 1. apply theme
-MonokaiTheme
+# MonokaiTheme
 
 # 2. 计算一些信息
 preCompute
@@ -20,76 +20,68 @@ figname=${figname}_geo
 # 新版本的gmt有时候psconvert转换pdf不成功，因此这里暂时先用png
 fmt=pdf
 gmt begin $figname $fmt
-    gmt psbasemap -JM$width_fig_x -JZ${width_fig_z}  -R$range_LonLatZ -Ba -BwsenZ -Bza+l"Depth (m)" -pz$angle_view  -TdjBL+o-3.5c/4.5c+w2c+f+lW,,S, --FONT_TITLE=12p --MAP_TITLE_OFFSET=0.1c 
-    gmt grdgradient ${bathy} -Az$angle_view -G$bathy_azm=nb/a -Ne0.2
-    gmt grdview ${bathy} -JZ -p -C$bathycpt -N${zmin}+g$color_profile -Qi -I$bathy_azm
+
+    # seafloor basemap
+    gmt psbasemap -JM$width_fig_x -JZ${width_fig_z}  -R$range_LonLatZ -Ba -BwsZ -Bza+l"Depth (m)" -pz$angle_view  -Tdj${prop_rose} --FONT_TITLE=12p --MAP_TITLE_OFFSET=0.1c 
+    # gmt grdgradient ${bathy} -Az$angle_view -G$bathy_azm=nb/a -Ne0.2
+    gmt grdview ${bathy} -JZ -p -C$bathycpt -N${zmin}+g$color_profile@$alpha_profile -Qi -I$bathy_azm -t$alpha_bathy 
     # gmt makecpt -C128 -T-5,0 -N > tmp.cpt
-    # gmt grdview ${bathy} -JZ -p -Ctmp.cpt -Qi -I$bathy_azm -t$alpha_bathy 
-    # add line
-    echo $lonc $lat_min >tmp_line
-    echo $lonc $lat_max >>tmp_line
-    awk 'NR>0 {print $1, $2}' tmp_line |gmt sample1d -I0.1k -AR | gmt grdtrack -G${bathy} | gmt psxyz  -JZ -p -W1,orange 
-    # add segments
-    awk 'NR>0 {print $1, $2}' $path_segments_SWIR/27.txt|gmt sample1d -I0.1k -AR | gmt grdtrack -G${bathy} | gmt psxyz  -JZ -p -W1,$lc_segments 
-    awk 'NR>0 {print $1, $2}' $path_segments_SWIR/28.txt|gmt sample1d -I0.1k -AR | gmt grdtrack -G${bathy} | gmt psxyz  -JZ -p -W1,$lc_segments 
-    
-    # segments text on seafloor
-    # 27
-    z_text=`awk 'NR==3 {print $1, $2}' $path_segments_SWIR/27.txt | grdtrack -G${bathy} | awk '{print $3}'`
-    awk 'NR==14 {print $1, $2+0, "Segment 27"}' $path_segments_SWIR/27.txt  | gmt pstext -JZ -p -Z$z_text -F+f20p,Helvetica-Bold,red=thinner,$lc_segments+jMC+a180
-    # 28
-    z_text=`awk 'NR==3 {print $1, $2-0.05}' $path_segments_SWIR/28.txt | grdtrack -G${bathy} | awk '{print $3}'`
-    awk 'NR==5 {print $1, $2-0.05, "Segment 28"}' $path_segments_SWIR/28.txt  | gmt pstext -JZ -p -Z$z_text -F+f20p,Helvetica-Bold,red=thinner,$lc_segments+jMC+a180
+    # gmt grdview ${bathy} -JZ -p -Ctmp.cpt -Qi -t$alpha_bathy 
+    gmt psscale -C$bathycpt -Dx$x_colorbar/$y_colorbar+w$width_colorbar/0.3c+h+jLB -Bxa2f0.4  -By+lkm  -V
+    # add text
+    z_text=`echo 20 -22 | gmt grdtrack -G${bathy}| awk '{print $3}'`
+    echo $z_text
+    echo "20" "-22" "Africa" | gmt pstext -JZ -Z$z_text -p -F+f20p,Helvetica-Bold,black=thinner,white+jMC+a0
+    #  add MOR
+    awk 'NR>0 {print $1, $2}' $path_MOR/SWIR.txt | gmt grdtrack -G${bathy} | gmt psxyz  -JZ -p -W1p,$lc_segments 
+    awk 'NR>0 {print $1, $2}' $path_MOR/SWIR.txt | gmt grdtrack -G${bathy} | gmt psxyz  -JZ -p -W1p,$lc_segments 
+    z_text=`awk 'NR==10 {print $1, $2}' $path_MOR/SWIR.txt | gmt grdtrack -G${bathy} | awk '{print $3}'`
+    awk 'NR==30 {print $1, $2-2, "SWIR"}' $path_MOR/SWIR.txt | gmt pstext -JZ -Z$z_text -p -F+f20p,Helvetica-Bold,red=thinner,white+jMC+a3
+    awk 'NR>0 {print $1, $2}' $path_MOR/CIR.txt | gmt grdtrack -G${bathy} | gmt psxyz  -JZ -p -W1p,$lc_segments 
+    awk 'NR>0 {print $1, $2}' $path_MOR/SEIR.txt | gmt grdtrack -G${bathy} | gmt psxyz  -JZ -p -W1p,$lc_segments 
+    z_text=`awk 'NR==15 {print $1, $2}' $path_MOR/SEIR.txt | gmt grdtrack -G${bathy} | awk '{print $3}'`
+    awk 'NR==15 {print $1, $2-2, "SEIR"}' $path_MOR/SEIR.txt | gmt pstext -JZ -Z$z_text -p -F+f20p,Helvetica-Bold,red=thinner,white+jMC+a-30
+    # add two profiles
+    echo $lon_longqi $lat_min $lat_max | awk '{printf "%f %f\n%f %f\n", $1, $2, $1, $3}' | gmt sample1d -I5k -AR | gmt project -C$lon_longqi/$lat_min -Q |awk '{print $1, $2, $3}' >profile_lon.xyd
+    awk '{print $1, $2}' profile_lon.xyd |gmt grdtrack -G${bathy} |  gmt psxyz -JZ -p -W1,$color_profile 
+    echo $lat_longqi $lon_min $lon_max | awk '{printf "%f %f\n%f %f\n", $1, $2, $1, $3}' | gmt sample1d -I5k -AR |awk '{print $2, $1}'| gmt project -C$lon_min/$lat_longqi -Q |awk '{print $1, $2, $4}'>profile_lat.xyd 
+    awk '{print $1, $2}' profile_lat.xyd |gmt grdtrack -G${bathy} | gmt psxyz -JZ -p -W1,$color_profile 
+    # 龙旗位置
+    echo $lon_longqi $lat_longqi | gmt grdtrack -G${bathy} | gmt psxyz -JZ -p -Sa0.5c -Gred -W1.5,yellow  
+    z_text=`echo $lon_longqi $lat_longqi |awk '{print $1, $2-5}' | gmt grdtrack -G${bathy}| awk '{print $3}'`
+    echo $z_text
+    echo $lon_longqi $lat_longqi "Longqi" |awk '{print $1, $2-5, $3}' | gmt pstext -JZ -Z$z_text -p -F+f20p,Helvetica-Bold,red=thinner,yellow+jMC+a0
     
     # profile along lon
-    gmt psbasemap -R$range_LonZLat -JX$width_fig_x/$width_fig_z -JZ$width_fig_y -BS -Ba --MAP_FRAME_PEN=1,black@100 --MAP_ANNOT_OFFSET=-0.2  -py$angle_view/$lat_min
-    echo $lonc $zc "lat_min"  | gmt pstext -JZ -p -F+f20p,Helvetica-Bold,red=thinner,$lc_segments+jMC+a0
-    gmt psbasemap -R$range_LonZLat -JX$width_fig_x/$width_fig_z -JZ$width_fig_y -BS -Ba --MAP_FRAME_PEN=1,black@100 --MAP_ANNOT_OFFSET=-0.2  -py$angle_view/$lat_max
-    echo $lonc $zc "lat_max"  | gmt pstext -JZ -p -F+f20p,Helvetica-Bold,red=thinner,$lc_segments+jMC+a0
-    # dxdy=0.01/0.2
-    # gausshill $range_LonZ $lonc $zc $dxdy $len_lon $len_z
-    # # 
-    # # make a mask
-    # polygon_profile=tmp_polygon_lonprofile.txt
-    # gmt grdtrack -ERT/LT+i0.2k+d -G$bathy | awk ' {print $1, $4}' >$polygon_profile
-    # echo $lon_min $zmin >> $polygon_profile
-    # echo $lon_max $zmin >> $polygon_profile
-    # gmt grdmask $polygon_profile -R$range_LonZ -I$dxdy -N0/1/1 -Gtmp_mask.nc=nb -V
-    # gmt grdmath tmp_data_simulation.nc tmp_mask.nc MUL = tmp_data.nc
-    # gmt grdclip tmp_data.nc -Gtmp_data_clip.nc -Sb0.0001/NaN -V
-    # # gmt grd2cpt tmp_data_clip.nc -Z -Ccpt-city/imagej/cequal >tmp.cpt
-    # gmt makecpt -Ccpt-city/imagej/cequal -T0.91/1/0.001 -Z >tmp.cpt
-    # gmt grdimage -JZ tmp_data_clip.nc -Ctmp.cpt -Q -p -t10
-    # gmt psxy $polygon_profile -JZ -p -W0.5,black -L 
-    # echo "$lonc $zc Melt" |gmt pstext -JZ -p -F+f15p,Helvetica-Bold,red=thinner,blue+jCM+a0 -Dj0c/0c
+    gmt psbasemap -R$range_LonZLat -JX$width_fig_x/$width_fig_z -JZ$width_fig_y -BS -Ba --MAP_FRAME_PEN=1,black@100 --MAP_ANNOT_OFFSET=$offset_ticklabel_lat  -py$angle_view/$pos_profile_lat
+    # plot s wave velocity on the profile
+    ymin_profile_vel=`grdinfo $nc_profile_lat| grep "y_min" | awk '{print $3}'`
+    ymax_profile_vel=`grdinfo $nc_profile_lat| grep "y_min" | awk '{print $5}'`
+    xrange_profile_vel=$lon_min/$lon_max 
+    range_profile_vel=$xrange_profile_vel/$ymin_profile_vel/$ymax_profile_vel
+    gmt grdedit $nc_profile_lat -R$range_profile_vel -Gtmp_vel.nc
+    gmt grd2cpt tmp_vel.nc -C$basecpt_vel -Z >vel.cpt 
+    gmt grdimage tmp_vel.nc -Cvel.cpt -JZ -py$angle_view/$lat_min -t$alpha_profile
+    gmt grdcontour tmp_vel.nc -C0.2 -JZ -p 
+    echo $lonc $zc "S Wave Velocity"  | gmt pstext -JZ -p -F+f20p,Helvetica-Bold,$lc_segments=thinner,red+jMC+a0
     
-
     # # profile along lat
-    gmt psbasemap -R$range_LatZLon -JX$width_fig_y/$width_fig_z -JZ$width_fig_x -BS -Ba --MAP_FRAME_PEN=1,black@100 --MAP_ANNOT_OFFSET=-0.2  -px$angle_view/$lon_min
-    echo $latc $zc "lon_min"  | gmt pstext -JZ -p -F+f20p,Helvetica-Bold,red=thinner,$lc_segments+jMC+a0
-    gmt psbasemap -R$range_LatZLon -JX$width_fig_y/$width_fig_z -JZ$width_fig_x -BS -Ba --MAP_FRAME_PEN=1,black@100 --MAP_ANNOT_OFFSET=-0.2  -px$angle_view/$lon_max
-    echo $latc $zc "lon_max"  | gmt pstext -JZ -p -F+f20p,Helvetica-Bold,red=thinner,$lc_segments+jMC+a0
-    # dxdy=0.01/0.2
-    # gausshill $range_LatZ $latc $zc $dxdy $len_lat $len_z
-    # # 
-    # # make a mask
-    # polygon_profile=tmp_polygon_latprofile.txt
-    # gmt grdtrack -ELT/LB+i0.2k+d -G$bathy | awk ' {print $2, $4}' >$polygon_profile
-    # echo $lat_min $zmin >> $polygon_profile
-    # echo $lat_max $zmin >> $polygon_profile
-    # gmt grdmask $polygon_profile -R$range_LatZ -I$dxdy -N0/1/1 -Gtmp_mask.nc=nb -V
-    # gmt grdmath tmp_data_simulation.nc tmp_mask.nc MUL = tmp_data.nc
-    # gmt grdclip tmp_data.nc -Gtmp_data_clip.nc -Sb0.0001/NaN -V
-    # # gmt grd2cpt tmp_data_clip.nc -Z -Ccpt-city/imagej/cequal >tmp.cpt
-    # gmt makecpt -Ccpt-city/imagej/cequal -T0.91/1/0.001 -Z >tmp.cpt
-    # gmt grdimage -JZ tmp_data_clip.nc -Ctmp.cpt -Q -p -t10
-    # gmt psxy $polygon_profile -JZ -p -W0.5,black -L 
-    # echo "$latc $zc AMC" |gmt pstext -JZ -p -F+f15p,Helvetica-Bold,red=thinner,blue+jCM+a0 -Dj0c/0c
-    # gmt psscale -Ctmp.cpt -Dx$x_colorbar/$y_colorbar+w$width_colorbar/0.3c+h+jLB -Bxa0.02f0.1  -By+lunit  -V
+    width_fig_y0=`echo $width_fig_y | awk '{print $1+0.3}'`
+    gmt psbasemap -R$range_LatZLon -JX$width_fig_y0/$width_fig_z -JZ$width_fig_x -BS -Ba --MAP_ANNOT_OFFSET=$offset_ticklabel_lon  -px$angle_view/$pos_profile_lon
+    ymin_profile_vel=`grdinfo $nc_profile_lon| grep "y_min" | awk '{print $3}'`
+    ymax_profile_vel=`grdinfo $nc_profile_lon| grep "y_min" | awk '{print $5}'`
+    xrange_profile_vel=$lat_min/$lat_max 
+    range_profile_vel=$xrange_profile_vel/$ymin_profile_vel/$ymax_profile_vel
+    gmt grdedit $nc_profile_lon -R$range_profile_vel -Gtmp_vel.nc
+    gmt grdimage tmp_vel.nc -Cvel.cpt -JZ -px$angle_view/$lon_min -t$alpha_profile
+    gmt grdcontour tmp_vel.nc -C0.1 -JZ -p
+    echo $latc $zc "SL2013sv Model"  | gmt pstext -JZ -p -F+f20p,Helvetica-Bold,$lc_segments=thinner,red+jMC+a0
+    gmt psscale -Cvel.cpt -Dx$x_colorbar_vel/$y_colorbar_vel+w$width_colorbar/0.3c+h+jRB -Bxa0.2f0.04  -By+lkm/s  -V
     
-
 gmt end
 open $figname.$fmt
 
 
 rm gmt.conf gmt.history tmp*
+
+
--- test/gmt_gzk/3d/SWIR/include.sh
+++ test/gmt_gzk/3d/SWIR/include.sh
@@ -27,28 +27,74 @@ function plot_gmtset()
 function figset()
 {
     figname=Test_3d
-    angle_view=225/25
+    # 旋转角度控制
+    azimuth=225
+    elevation=20
+    angle_view=$azimuth/$elevation
     width_fig_x=10
     width_fig_y=10
     width_fig_z=7
     # colorbar
     x_colorbar=-0.8
-    y_colorbar=0.2
-    width_colorbar=5
+    y_colorbar=7.5
+    x_colorbar_vel=12
+    y_colorbar_vel=0.2
+    width_colorbar=4
     # alpha
     alpha_profile=50
-    alpha_bathy=100    #0表示不透明，100表示全透明,地形上面的覆盖层的透明度
+    alpha_bathy=5    #0表示不透明，100表示全透明,地形上面的覆盖层的透明度
     # color
-    color_profile=lightblue@$alpha_profile     #三维剖面颜色
-    color_secondaryProfile=gray
+    color_profile=lightgray     #三维剖面颜色
+    color_secondaryProfile=white
     lc_segments=white
     # move
     move_x=0
     move_y=0
 }
+function getProfilePos()
+{
+    # 第一种情况
+    if [ "$azimuth" -le "90" ]; then
+    echo "lat_max lon_max"
+    pos_profile_lon=$lon_max 
+    pos_profile_lat=$lat_max
+    offset_ticklabel_lat=0
+    offset_ticklabel_lon=-0.2
+    prop_rose=MR+o-4c/0c+w2c+f+lW,,S,
+    # 第二种情况
+    elif [ "$azimuth" -le "180" ]; then
+    echo "lat_max lon_min"
+    pos_profile_lon=$lon_max
+    pos_profile_lat=$lat_min
+    offset_ticklabel_lat=0
+    offset_ticklabel_lon=0
+    prop_rose=BC+o0c/-4c+w2c+f+lW,,S,
+    # fi
+    # 第三种情况
+    elif [ "$azimuth" -le "270" ]; then
+    echo "lat_min lon_min"
+    pos_profile_lon=$lon_min
+    pos_profile_lat=$lat_min
+    offset_ticklabel_lat=0
+    offset_ticklabel_lon=0
+    prop_rose=ML+o-4.5c/0c+w2.5c+f+lW,,,N
+    # fi
+    # 第四种情况
+    elif [ "$azimuth" -gt "270" ]; then
+    echo "lat_min lon_max"
+    pos_profile_lon=$lon_min 
+    pos_profile_lat=$lat_max
+    offset_ticklabel_lat=0
+    offset_ticklabel_lon=-0.2
+    prop_rose=TC+o0c/-4c+w2c+f+l,E,,N
+    fi
+}
 function datapath()
 {
     path_segments_SWIR=/MyData/Research/2-Paper/4_Numerical_Longqi/data/segments/
+    path_MOR=/MyData/Research/2-Paper/4_Numerical_Longqi/data/MOR
+    nc_profile_lon=profile_lon.xyd_vel.nc 
+    nc_profile_lat=profile_lat.xyd_vel.nc
 }
 function dataset()
 {
@@ -56,8 +102,8 @@ function dataset()
     xmax=100
     ymin=0
     ymax=50
-    zmin=-50
-    zmax=0
+    zmin=-400
+    zmax=-2
     xc=`echo $xmin $xmax | awk '{print $1+($2-$1)/2}'`
     yc=`echo $ymin $ymax | awk '{print $1+($2-$1)/2}'`
     zc=`echo $zmin $zmax | awk '{print $1+($2-$1)/2}'`
@@ -66,18 +112,22 @@ function dataset()
     range_z=$zmin/$zmax
     range_xyz=$range_xy/$range_z
     # get bathymetry of a rigion
-    lon_min=-10
-    lon_max=70
-    lat_min=-60
-    lat_max=-10
+    lon_min=0
+    lon_max=90
+    lat_min=-67
+    lat_max=-18
+    lon_longqi=49
+    lat_longqi=-37
+    getProfilePos    #根据旋转角度和经纬度范围设置剖面显示的位置
     len_lon=`echo $lon_min $lon_max | awk '{print ($2-$1)}'`
     len_lat=`echo $lat_min $lat_max | awk '{print ($2-$1)}'`
-    scale_z=50  #z轴与横向的尺度比例，用于计算z轴的长度
+    scale_z=7  #z轴与横向的尺度比例，用于计算z轴的长度
     bathy=bathy_swir.nc
     bathycpt=bathy.cpt
     bathy_azm=bathy.azm
-    cpt_base=cpt-city/mpl/viridis
-    cutBathy $bathy $cpt_base
+    cpt_base=cpt-city/mby/mby
+    basecpt_vel=cpt-city/arendal/temperature
+    # cutBathy $bathy $cpt_base
     range_LonZ=$lon_min/$lon_max/$zmin/$zmax
     range_LatZ=$lat_min/$lat_max/$zmin/$zmax
     range_LonLatZ=$lon_min/$lon_max/$lat_min/$lat_max/$zmin/$zmax
@@ -93,7 +143,7 @@ function cutBathy()
     incpt=$2
     etopo1=/MyData/Research/0-Phd/Data/GlobalGeophysicalData/ETOPO1_Bed_g_gdal.nc
     range_lonlat=$lon_min/$lon_max/$lat_min/$lat_max 
-    gmt grdsample $etopo1 -R$range_lonlat -G$etopo_save -I5m
+    gmt grdsample $etopo1 -R$range_lonlat -G$etopo_save -I1m
     gmt grdmath $etopo_save 0.001 MUL = $etopo_save
     # make cpt
     gmt grd2cpt $etopo_save -C$incpt -Z >$bathycpt
@@ -108,18 +158,7 @@ function MonokaiTheme()
     gmt set MAP_FRAME_PEN=thicker,white
     gmt set MAP_TICK_PEN_PRIMARY=,white
     # --------color set -------------------
-    # color_G_eq_lon=white             #经度地震分布点的填充颜色
-    # color_W_eq_lon=yellow               #经度剖面地震分布点的edge颜色
-    # color_G_eq_lat=orange               #纬度剖面地震分布点的填充颜色
-    # color_W_eq_lat=white                #纬度剖面地震分布点的edge颜色
-    # color_G_eq_sf=$color_G_eq_lon       #海底地震分布点的填充颜色
-    # color_W_eq_sf=$color_W_eq_lon       #海底地震分布点的edge颜色
-    # lc_brittle_ductile=MEDIUMVIOLETRED  #脆性-韧性转换带曲线的颜色
-    # color_profile=black@20              #主要剖面的颜色
-    # color_secondaryProfile=117/113/94    #次要剖面的颜色
-    # color_DF=249/38/114                  #DF曲线颜色
-    # color_fault=166/2226/46             #小断层颜色
-    # color_continent=black
+    lc_segments=gray
 
     figname=${figname}_DarkTheme
 }
